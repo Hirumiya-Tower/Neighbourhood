a856693fbe56ac6a5bf32affa2cadb1e95578cc3
Hirumiya-Tower <oiauraoierwva@gmail.com>
2025-04-11 18:44:22 +0900

    ダウンロード強制を検討

--- src/app/api/getFile/[name]/route.ts
+++ src/app/api/getFile/[name]/route.ts
@@ -34,7 +34,7 @@ export async function GET(
 
         const headers = {
             "Content-Type": "application/pdf",
-            "Content-Disposition": `inline; filename="${path.basename(fileName)}"`,
+            "Content-Disposition": `attachment; filename="${path.basename(fileName)}"`,
             "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
             "Expires": "0",
             "Pragma": "no-cache",
